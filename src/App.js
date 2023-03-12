7edf7f89b5301757453a19c091533abd74950a22
SolForte <mateusbrandao1234@gmail.com>
2023-03-12 03:15:06 -0300

    Feat: Routes e route porém sem funcionalidade

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import styled from "styled-components"
 import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
@@ -7,12 +8,15 @@ import SuccessPage from "./pages/SuccessPage/SuccessPage"
 export default function App() {
     return (
         <>
+        <BrowserRouter>
            <NavContainer>CINEFLEX</NavContainer>
-
-            {/* <HomePage /> */}
-            {/* <SeatsPage /> */}
-            <SessionsPage />
-            {/* <SuccessPage /> */}
+           <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route  path="/sessoes/:idFilme" element={<SessionsPage />}/>
+                <Route  path="/assentos/:idSessao" element={<SeatsPage />}/>
+                <Route  path="/sucesso" element={<SuccessPage />}/>
+           </Routes>
+        </BrowserRouter>
         </>
     )
 }
